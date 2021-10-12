aac7afdabe586eb4196e9dfb3831026ca060dd1a
Robika006 <91243944+Robika006@users.noreply.github.com>
2021-10-12 09:46:49 +0300

    Add files via upload

--- nXnmatrixprimelemei/main.cpp
+++ nXnmatrixprimelemei/main.cpp
@@ -6,20 +6,20 @@ int main()
 {
     int v[100][100],n,s=0,i,j,oszto;
     bool prim;
-    cout<<"n=";
-    cin>>n;
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
+    cout <<"n=";
+    cin >>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
                 prim=true;
                 cin >> v[i][j];
-                for(oszto=0; oszto<v[i][j]/2+1; oszto++){
+                for(int oszto=0; oszto<v[i][j]/2+1; oszto++){
                     if (v[i][j] % oszto==0){
                         prim=false;
                         oszto=v[i][j]/2+1;
                     }
                 }
                 if(prim==true){
-                    s=s+v[i[j];
+                    s+=v[i][j];
                 }
 
         }
